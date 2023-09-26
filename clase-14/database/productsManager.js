c927ed25d7566b7cfbc888da882200024172466d
matu.dev <matu.a.gimenez2002@gmail.com>
2023-09-25 22:03:30 -0300

    clase 14

--- clase-14/database/productsManager.js
+++ clase-14/database/productsManager.js
@@ -5,7 +5,14 @@ const products =[
         stock: 5,
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat quam incidunt non expedita sequi provident unde adipisci. Iure, at ipsum non, odit corporis nemo quia atque animi eius fuga beatae?',
         id: 1
-    }
+    },
+    {
+        name: 'tv phillips 32"',
+        price: 200000,
+        stock: 2,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat quam incidunt non expedita sequi provident unde adipisci. Iure, at ipsum non, odit corporis nemo quia atque animi eius fuga beatae?',
+        id: 2
+    },
 ]
 
 const getAllProducts = () => {
@@ -16,5 +23,16 @@ const getProductById = (id) => {
 
 }
 
+const deleteProductById = (id) =>{
+    let posProductoBuscado = products.findIndex(product => product.id === Number(id))
+    products.splice(posProductoBuscado, 1)
+}
+const updateStockById = (id, stock) =>{
+    const productToUpdate = products.find(product => product.id === Number(id))
+    productToUpdate.stock =  stock
+    
+}
 
-module.exports = { getAllProducts }
+
+
+module.exports = { getAllProducts, deleteProductById, updateStockById}
--- clase-14/router/routerProducts.js
+++ clase-14/router/routerProducts.js
@@ -1,11 +1,24 @@
 const express = require('express')
-const { getAllProducts } = require('../database/productsManager')
+const { getAllProducts, deleteProductById, updateStockById } = require('../database/productsManager')
 
 const router = express.Router() 
 
 
 router.get('/', (req, res) =>{
     res.json({ok: true, products: getAllProducts()})
+})
+
+router.delete('/:id', (req, res) =>{
+    const {id} = req.params
+    deleteProductById(id)
+    res.json({ok: true, message: 'Producto eliminado', products: getAllProducts()})
+})
+
+router.put('/:id/:stock', (req, res) =>{
+    const {id, stock} = req.params
+
+    updateStockById(id, stock)
+    res.json({ok: true, message: 'Producto actualizado', products: getAllProducts()})
 })
 
 module.exports = router
