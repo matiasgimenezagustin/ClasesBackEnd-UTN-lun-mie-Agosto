92996a29c26d5a9c85ea201378dbf90784c72d6b
matu.dev <matu.a.gimenez2002@gmail.com>
2023-10-11 22:02:03 -0300

    clase 19

--- clase-19/app.js
+++ clase-19/app.js
@@ -3,6 +3,7 @@ const session = require('express-session')
 const mongoose =  require('mongoose')
 const hbs = require("hbs")
 const dotenv = require('dotenv')
+const bcrypt = require('bcrypt')
 dotenv.config()
 
 
@@ -30,6 +31,52 @@ app.set('views', __dirname + '/views')
 const ADMIN_USERNAME = process.env.ADMIN_USERNAME
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD
 const DB_NAME = 'eCommerceUTN_Noche_LM'
+
+let original_password = process.env.ADMIN_PASSWORD
+
+let hashedPassword
+
+
+bcrypt.genSalt(10, (err, salt) =>{
+    if(err){
+        console.log("Hubo un error al generar la sal")
+    }
+    else{
+        bcrypt.hash(original_password, salt, (err, hash) =>{
+            if(err){
+                console.log('hubo un error al hashear la contraseña')
+            }
+            else{
+                hashedPassword = hash
+                console.log('Su contraseña ha sido hasheada correctamente:', hashedPassword)
+            }
+            
+        })
+    }
+})
+
+const verificarContraseña = async (password) =>{ //REVISAR
+    let isCorrect
+    await bcrypt.compare(password, hashedPassword, (err, result) =>{
+        if(err){
+            isCorrect = false
+            console.log('hola')
+        }
+        else if(result){
+            isCorrect = true
+        }
+        else{
+            isCorrect = false
+        }
+    })
+    console.log(isCorrect)
+    return isCorrect
+}
+
+
+
+
+
 const DB_PASSWORD = process.env.DB_PASSWORD
 const URL_CONNECTION = `mongodb+srv://matu_dev:${DB_PASSWORD}@cluster0.xyjacy2.mongodb.net/${DB_NAME}`
 
@@ -51,6 +98,13 @@ database.once('open', () =>{
 const User = mongoose.model('User', {
     username: String, 
     password: String
+})
+
+const Producto = mongoose.model('Producto', {
+    precio: Number, 
+    nombre: String,
+    stock: Number,
+    descripcion: String
 })
 
 //Endpoints:
@@ -90,7 +144,7 @@ app.get('/error', (req, res) => {
 
 app.post('/login', async (req, res)=>{
     const {username, password} = req.body
-    if(username ==  ADMIN_USERNAME && password == ADMIN_PASSWORD){
+    if(username ==  ADMIN_USERNAME && verificarContraseña(password)){
         req.session.isAdmin = true
         req.session.user = {username: 'administrador'}
         return res.redirect('/')
