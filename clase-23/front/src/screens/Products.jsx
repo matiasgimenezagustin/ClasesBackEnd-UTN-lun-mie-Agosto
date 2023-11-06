5421e4a9cea62f3a3cf79af50352f2b7fd50f192
matu.dev <matu.a.gimenez2002@gmail.com>
2023-11-06 20:43:48 -0300

    clase final

--- clase-23/front/src/screens/Products.jsx
+++ clase-23/front/src/screens/Products.jsx
@@ -10,8 +10,19 @@ const Products = () => {
     }, [])
     console.log(products)
   return (
-    <div>products</div>
+    <div>
+        {products.length != 0  && products.map((product) =>(
+            <div key={product._id}>
+                {product.thumbnail && <img src={'http://localhost:3040/images/' + product.thumbnail}  />}
+                
+                <h2>{product.nombre}</h2>
+
+            </div>
+        ))}
+    </div>
   )
 }
 
 export default Products
+
+/* Si entar los productos, renderizarlos por la pantalla (usando un map) */
